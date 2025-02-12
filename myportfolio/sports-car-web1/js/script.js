acaf8a8cf857ff441959318fdb8cf55c6cce4cff
Jnails99 <87135477+Jnails99@users.noreply.github.com>
2025-02-12 07:48:50 -0500

    load

---
+++ myportfolio/sports-car-web1/js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav')
+    menu.classList.toggle('active');
+    nav.classList.toggle('active')
+}
